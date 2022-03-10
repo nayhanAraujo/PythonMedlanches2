6e548f9762adf35011870050d556658dce4e6953
Nayhan Araujo <99093994+nayhanAraujo@users.noreply.github.com>
2022-03-09 22:00:37 -0300

    Validando login
    Seguindo  o tutorial do video no youtube para validar o login

--- medlanchesfeliz/settings.py
+++ medlanchesfeliz/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'medlanchesfeliz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +81,7 @@ DATABASES = {
         'PORT': '3306',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'admin'
+        'PASSWORD': ''
     }
 }
 
@@ -136,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
